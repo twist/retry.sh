1d55206fede94a1e2da6bfac3d5abeaa0a13d325
twist <bellee@webit.de>
2010-01-29 16:18:53 +0100

    first draft

--- retry.sh
+++ retry.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 
+while [ "1" = "1" ]
+do
+      echo "executing"
+        ./may_fail.sh
 
+      if [ "$?" = "0" ] #success!
+      then
+              exit
+      fi
+      echo "... failed"
+
+done
 
